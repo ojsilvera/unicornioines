4d27cbd773c207b8af95c5cac59ce3d6d3ced9eb
mardurango <mlcardona85@mmmisena.edu.co>
2019-09-11 19:00:19 -0500

    agregue encabezado en el inicio

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -2,8 +2,7 @@
 
         <div class="flex-child">
 
-                <p>
-
+                <p> <h6>
                 El INES es una herramienta que le permite a las instituciones de educación superior identificar las condiciones
                 en que se encuentran con respecto a la atención a la diversidad, analizar sus fortalezas y oportunidades
                 de mejoramiento y tomar decisiones que cualifiquen el aprendizaje, la participación y la convivencia
@@ -11,14 +10,12 @@
                 según el cual la “inclusión es un conjunto de procesos orientados a aumentar la participación de los estudiantes
                 en la cultura, los currículos y las comunidades”.
 
-                </p>
+                </h6></p>
 
-                <p>
-
+                <p> <h6>
                 El objetivo de esta herramienta es fomentar una educación de calidad, promoviendo estrategias de
                 educación inclusiva dentro de las IES, como parte central de la política institucional.
-
-                </p>
+                </h6></p>
 
         </div>
 
